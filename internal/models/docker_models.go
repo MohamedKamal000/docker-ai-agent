352a57fa2cf26bfbddb992ff8ce128592d3a7701
M0Leo <mohamedleo404@gmail.com>
2026-05-17 17:28:57 +0300

    feat: update docker types

--- internal/models/docker_models.go
+++ internal/models/docker_models.go
@@ -1,8 +1,45 @@
 package models
 
+import "time"
+
 type Container struct {
-	Name   string `json:"name"`
-	Image  string `json:"image"`
-	Status string `json:"status"`
-	Id     string `json:"id"`
+	Id     string   `json:"id"`
+	Name   string   `json:"name"`
+	Image  string   `json:"image"`
+	Status string   `json:"status"`
+	State  string   `json:"state"`
+	Created time.Time `json:"created"`
+}
+
+type PortMapping struct {
+	HostIP        string `json:"host_ip,omitempty"`
+	HostPort      string `json:"host_port"`
+	ContainerPort string `json:"container_port"`
+	Protocol      string `json:"protocol"` 
+}
+
+type ContextSnapshot struct {
+    Containers []Container `json:"containers"`
+    Images     []Image     `json:"images"`
+    Volumes    []Volume    `json:"volumes"`
+    Networks   []Network   `json:"networks"`
+    CapturedAt time.Time   `json:"captured_at"`
+}
+
+type Image struct {
+    ID   string   `json:"id"`
+    Tags []string `json:"tags"`
+    Size int64    `json:"size"`
+    Created time.Time `json:"created"`
+}
+
+type Volume struct {
+    Name   string `json:"name"`
+    Driver string `json:"driver"`
+}
+
+type Network struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
+    Mode string `json:"mode"`
 }
